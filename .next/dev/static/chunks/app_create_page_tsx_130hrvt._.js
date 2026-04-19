a1ad404ca85af48751c059119f38b18391f48486
“DennistheBusiness” <Dennisthebusiness@gmail.com>
2026-04-19 12:29:06 -0400

    Update .gitignore and remove .next from tracking

--- .next/dev/static/chunks/app_create_page_tsx_130hrvt._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_11-6jo4._.js","static/chunks/_0nmzv5x._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_memorization_[id]_page_tsx_130hrvt._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_12inh~i._.js","static/chunks/node_modules_12y9xha._.js"],
-    source: "dynamic"
-});
